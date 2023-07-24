0efc8fed21ee6953a20f89268558b5b836a31720
yrojx <yossansr06@gmail.com>
2023-07-25 06:57:44 +0700

    feat: add icons component with lucide-react

---
+++ src/components/icons.tsx
@@ -0,0 +1,10 @@
+import { LayoutDashboard, ArrowLeftRight, Receipt, UserSquare2 } from 'lucide-react'
+
+export const Icons = {
+  LayoutDashboard,
+  ArrowLeftRight,
+  Receipt,
+  UserSquare2
+}
+
+export default Icons
