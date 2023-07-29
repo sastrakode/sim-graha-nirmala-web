77f0f5fbc1335c5476feec821a51291a46f7b88b
Restyled.io <commits@restyled.io>
2023-07-29 12:47:24 +0000

    Restyled by pg_format

--- drizzle/0000_strong_miracleman.sql
+++ drizzle/0000_strong_miracleman.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS "house" (
-	"id" bigserial PRIMARY KEY NOT NULL,
-	"code" text NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone,
-	CONSTRAINT "house_code_unique" UNIQUE("code")
+    "id" bigserial PRIMARY KEY NOT NULL,
+    "code" text NOT NULL,
+    "created_at" timestamp with time zone DEFAULT now() NOT NULL,
+    "updated_at" timestamp with time zone,
+    CONSTRAINT "house_code_unique" UNIQUE ("code")
 );
+
