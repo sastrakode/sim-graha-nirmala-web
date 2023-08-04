9b6ba85246e2a73c7892e305cc8d208da91f21fb
Achmad Chun Chun <anonychun@gmail.com>
2023-08-04 15:47:23 +0700

    add transaction handler

--- src/server/web/handler.ts
+++ src/server/web/handler.ts
@@ -1,6 +1,11 @@
 import { ApiError } from "@/server/models/exceptions/api"
 import { NextRequest } from "next/server"
 import { sendErrors, sendErrorServer } from "./response"
+import * as schema from "../db/schema"
+import { PgTransaction } from "drizzle-orm/pg-core"
+import { PostgresJsQueryResultHKT } from "drizzle-orm/postgres-js"
+import { ExtractTablesWithRelations } from "drizzle-orm"
+import { db } from "../db"
 
 interface Handler<T = any> {
   (req: NextRequest, event: { params: any }): T
@@ -10,6 +15,32 @@ export function defineHandler(handler: Handler) {
   return async (req: NextRequest, event: { params: any }) => {
     try {
       return await handler(req, event)
+    } catch (error) {
+      if (error instanceof ApiError) {
+        return sendErrors(error.code, error.errors)
+      }
+
+      return sendErrorServer()
+    }
+  }
+}
+
+interface TransactionHandler<T = any> {
+  (
+    tx: PgTransaction<
+      PostgresJsQueryResultHKT,
+      typeof schema,
+      ExtractTablesWithRelations<typeof schema>
+    >,
+    req: NextRequest,
+    event: { params: any },
+  ): T
+}
+
+export function defineTransactionHandler(handler: TransactionHandler) {
+  return async (req: NextRequest, event: { params: any }) => {
+    try {
+      return await db().transaction(async (tx) => await handler(tx, req, event))
     } catch (error) {
       if (error instanceof ApiError) {
         return sendErrors(error.code, error.errors)
