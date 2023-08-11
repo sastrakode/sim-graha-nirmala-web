4a77b2a40ea15af50a9629b4330bf7658305bb4a
Restyled.io <commits@restyled.io>
2023-08-11 17:32:26 +0000

    Restyled by pg_format

--- drizzle/0001_flaky_golden_guardian.sql
+++ drizzle/0001_flaky_golden_guardian.sql
@@ -1,14 +1,23 @@
 CREATE TABLE IF NOT EXISTS "announcement_category" (
-	"id" bigserial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone,
-	CONSTRAINT "announcement_category_name_unique" UNIQUE("name")
+    "id" bigserial PRIMARY KEY NOT NULL,
+    "name" text NOT NULL,
+    "created_at" timestamp with time zone DEFAULT now() NOT NULL,
+    "updated_at" timestamp with time zone,
+    CONSTRAINT "announcement_category_name_unique" UNIQUE ("name")
 );
+
 --> statement-breakpoint
-ALTER TABLE "announcement" ADD COLUMN "announcement_category_id" bigint NOT NULL;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "announcement" ADD CONSTRAINT "announcement_announcement_category_id_announcement_category_id_fk" FOREIGN KEY ("announcement_category_id") REFERENCES "announcement_category"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "announcement"
+    ADD COLUMN "announcement_category_id" bigint NOT NULL;
+
+--> statement-breakpoint
+DO $$
+BEGIN
+    ALTER TABLE "announcement"
+        ADD CONSTRAINT "announcement_announcement_category_id_announcement_category_id_fk" FOREIGN KEY ("announcement_category_id") REFERENCES "announcement_category" ("id") ON DELETE NO action ON UPDATE NO action;
 EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
+    WHEN duplicate_object THEN
+        NULL;
+END
+$$;
+
