3e52945f3a70edd3c1d27b45975f61c4f34cb6e7
yrojx <yossansr06@gmail.com>
2023-08-12 17:30:42 +0700

    add handle fetch functionality

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,14 @@
 import { getCookie } from "cookies-next"
+import { House, Occupant, OccupantLogin } from "./model"
 
 const baseURL = "http://localhost:3000/api/v1"
 const isServer = typeof window === "undefined"
+
+export const fetchErrorString = "errors"
+
+interface FetchError {
+  errors: string[]
+}
 
 const getToken = async () => {
   let token: string
@@ -14,28 +21,56 @@ const getToken = async () => {
   return token
 }
 
-export async function occupantLogin(body: {}) {
-  const res = await fetch(`${baseURL}/auth/occupant/login`, {
-    method: "POST",
-    body: JSON.stringify(body),
-  })
+async function handleFetch<T>(
+  url: string,
+  requestOptions?: RequestInit,
+): Promise<T | FetchError> {
+  try {
+    const res = await fetch(url, requestOptions)
 
-  return res.json()
+    const resJson = await res.json()
+
+    // Sometimes we need errors fields (example: login)
+    if (!res.ok) {
+      if (resJson.errors) return { errors: resJson.errors }
+      else {
+        throw new Error("Something went wrong")
+      }
+    }
+
+    return resJson.data
+  } catch (error) {
+    throw new Error("Something went wrong")
+  }
 }
 
-export async function getOccupant(id: string) {
-  const token = await getToken()
-  if (token) {
-    try {
-      const res = await fetch(`${baseURL}/occupant/${id}`, {
-        headers: {
-          Authorization: token,
-        },
-      })
+export async function occupantLogin(body: {}): Promise<
+  OccupantLogin | FetchError
+> {
+  const res = await handleFetch<OccupantLogin>(
+    `${baseURL}/auth/occupant/login`,
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+    },
+  )
 
-      return res.json()
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  return res
+}
+
+export async function getHouse(id: string): Promise<House | FetchError> {
+  const res = await handleFetch<House>(`${baseURL}/house/${id}`)
+  return res
+}
+
+export async function getOccupant(id: string): Promise<Occupant | FetchError> {
+  const token = await getToken()
+
+  const res = await handleFetch<Occupant>(`${baseURL}/occupant/${id}`, {
+    headers: {
+      Authorization: token,
+    },
+  })
+
+  return res
 }
