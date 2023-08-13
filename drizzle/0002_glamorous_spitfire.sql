e37eb57d973b2f8eac8aa604a7c964acf53dabf1
Restyled.io <commits@restyled.io>
2023-08-13 15:34:32 +0000

    Restyled by pg_format

--- drizzle/0002_glamorous_spitfire.sql
+++ drizzle/0002_glamorous_spitfire.sql
@@ -1,5 +1,12 @@
-ALTER TABLE "cashflow" DROP CONSTRAINT "cashflow_cashflow_type_id_cashflow_type_id_fk";
+ALTER TABLE "cashflow"
+    DROP CONSTRAINT "cashflow_cashflow_type_id_cashflow_type_id_fk";
+
 --> statement-breakpoint
-DROP TABLE "cashflow_type";--> statement-breakpoint
-ALTER TABLE "cashflow" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "cashflow" DROP COLUMN IF EXISTS "cashflow_type_id";
+DROP TABLE "cashflow_type";
+
+--> statement-breakpoint ALTER TABLE "cashflow"
+    ADD COLUMN "title" text NOT NULL;
+
+--> statement-breakpoint ALTER TABLE "cashflow"
+    DROP COLUMN IF EXISTS "cashflow_type_id";
+
