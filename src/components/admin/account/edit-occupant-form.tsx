f45e1ed91458272e658df92d3d401037bda64300
yrojx <yossansr06@gmail.com>
2023-08-26 00:36:13 +0700

    fix: add navigation

--- src/components/admin/account/edit-occupant-form.tsx
+++ src/components/admin/account/edit-occupant-form.tsx
@@ -28,6 +28,7 @@ import { useState } from "react"
 import { editOccupantFormSchema } from "@/lib/schema"
 import { normalizePhone } from "@/lib/utils"
 import { putOccupant } from "@/lib/api"
+import { useRouter } from "next/navigation"
 
 const formSchema = editOccupantFormSchema
 
@@ -38,6 +39,8 @@ export function EditOccupantForm({
   occupant: OccupantResponse
   houses: HouseResponse[]
 }) {
+  const router = useRouter()
+
   const [filteredHouses, setFilteredHouses] = useState(
     filterHouses(occupant.role as occupantRoleType),
   )
@@ -73,6 +76,7 @@ export function EditOccupantForm({
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     await putOccupant(occupant.id.toString(), values)
+    router.push("/admin/account")
   }
 
   return (
--- src/components/admin/account/edit-staff-form.tsx
+++ src/components/admin/account/edit-staff-form.tsx
@@ -26,10 +26,12 @@ import { staffRoleType, staffRoleTypes } from "@/lib/constants"
 import { editStaffFormSchema } from "@/lib/schema"
 import { normalizePhone } from "@/lib/utils"
 import { putStaff } from "@/lib/api"
+import { useRouter } from "next/navigation"
 
 const formSchema = editStaffFormSchema
 
 export function EditStaffForm({ staff }: { staff: StaffResponse }) {
+  const router = useRouter()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -42,6 +44,7 @@ export function EditStaffForm({ staff }: { staff: StaffResponse }) {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     await putStaff(staff.id.toString(), values)
+    router.push("/admin/account")
   }
 
   return (
