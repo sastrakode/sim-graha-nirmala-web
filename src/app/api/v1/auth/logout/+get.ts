156e1601ad758aadce482757a17822c73a029e4f
yrojx <yossansr06@gmail.com>
2023-09-24 13:01:14 +0700

    fix: explicitly set force-dynamic

--- src/app/api/v1/auth/logout/+get.ts
+++ src/app/api/v1/auth/logout/+get.ts
@@ -2,6 +2,8 @@ import { defineHandler } from "@/server/web/handler"
 import { sendNoContent } from "@/server/web/response"
 import { cookies } from "next/headers"
 
+export const dynamic = "force-dynamic"
+
 export const GET = defineHandler(async (req) => {
   const res = sendNoContent()
   cookies()
